24466ecc22bf9a94f6d7e2ec4c2cc653fae768d7
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-01-19 22:52:56 +0000

    AddPushFunction/CE

--- monty.h
+++ monty.h
@@ -50,7 +50,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/**void push(stack_t **stack, unsigned int line_num);**/
+void push(stack_t **stack, unsigned int line_num);
 void pall(stack_t **stack, unsigned int line_num);
 void pint(stack_t **stack, unsigned int line_num);
 void pop(stack_t **stack, unsigned int line_num);
--- opcodes.c
+++ opcodes.c
@@ -22,6 +22,43 @@ void pall(stack_t **stack, unsigned int line_num)
 }
 
 /**
+ * push - add node to the stack
+ * @stack: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void push(stack_t **stack, unsigned int line_num)
+{
+	int n, j = 0, flag = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", line_num);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*stack);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", line_num);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		add_node(stack, n);
+	else
+		add_node_end(stack, n);
+}
+
+/**
  * pop - Remove the top element of the stack
  * @stack: Double pointer to the top of the stack
  * @line_num: The line of the file the command was found
