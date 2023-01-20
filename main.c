be0de40ff4a10f3d4aae4907486a30a8b7d0a340
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-01-20 00:02:30 +0000

    ipdate main.c avec struct bus

--- main.c
+++ main.c
@@ -11,41 +11,37 @@ bus_t bus = {NULL, NULL, NULL, 0};
 int main(int argc, char *argv[])
 {
 	FILE *file;
-	char *line, *command;
-	size_t size, line_num;
-	stack_t *stack;
-	ssize_t read = 0;
+	char *line;
+	size_t size = 0;
+	unsigned int line_num = 0;
+	stack_t *stack = NULL;
+	ssize_t read = 1;
 
-	stack = NULL;
-	line = NULL;
-	size = 0;
-	line_num = 1;
 	if (argc != 2)
 	{
 		printf("usage: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
-	if (file == NULL)
+	bus.file = file;
+	if (!file)
 	{
 		printf("Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	read = getline(&line, &size, file);
-	while (read != -1)
+	while (read > 0)
 	{
-		command = find_command(line, &stack, line_num);
-		if (strcmp(command, "nop"))
-			check_codes(command, &stack, line_num);
-		if (ret_and_q.opcode_return != 0)
-		{
-			free_and_exit(line, file, stack);
-		}
-		line_num++;
+		line = NULL;
 		read = getline(&line, &size, file);
+		bus.line = line;
+		line_num++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(line);
 	}
 	free_stack(stack);
-	free(line);
 	fclose(file);
 	return (0);
 }
